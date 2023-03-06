4cc70edf90d110d8759207b9dc75016a946115d6
developer-KOR <126733809+developer-KOR@users.noreply.github.com>
2023-03-07 08:50:28 +0900

    Add files via upload
    계산기 quotes 추가

---
+++ js/phrases.js
@@ -0,0 +1,55 @@
+const quotes = [
+  {
+    quote: "I never dreamed about success, I worked for it.",
+    author: "Estee Lauder",
+  },
+  {
+    quote: "Do not try to be original, just try to be good.",
+    author: "Paul Rand",
+  },
+  {
+    quote: "Do not be afraid to give up the good to go for the great",
+    author: "John D. Rockefeller",
+  },
+  {
+    quote:
+      "If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    quote:
+      "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas Edison",
+  },
+  {
+    quote:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+];
+
+const phrase = document.querySelector("#quote p:first-child");
+const author = document.querySelector("#quote p:last-child");
+const random = quotes[Math.floor(Math.random() * quotes.length)];
+
+phrase.innerText = random.quote;
+author.innerHTML = `- <strong>${random.author}</strong> -`;
