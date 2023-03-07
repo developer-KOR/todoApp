f6f9f4e8eaae5802494148f3cc7cfce356ff3d1c
developer-KOR <126733809+developer-KOR@users.noreply.github.com>
2023-03-07 15:43:36 +0900

    Add files via upload

--- js/audio.js
+++ js/audio.js
@@ -17,12 +17,32 @@ const audioIndex = Math.floor( Math.random() * audioFile.length )
 const audioWrap = document.querySelector("#wrap .audio");
 const audio = document.createElement("audio");
 const audioTitle = document.createElement("figcaption");
+const volumeBtn = document.querySelector(".volume_button");
+
 const AUTO_PLAY = "autoplay";
 
 audioTitle.innerText = audioFile[audioIndex].title;
 audio.src = audioFile[audioIndex].src;
-audio.autoplay = "autoplay";
+audio.autoplay = AUTO_PLAY;
 
 audioWrap.prepend(audioTitle);
 audioWrap.appendChild(audio);
 
+function volume(e){
+  e.preventDefault();
+  
+  if ( volumeBtn.classList.contains("off") ){
+    audio.play();
+    volumeBtn.innerText = "🔊";
+    volumeBtn.classList.remove("off")
+    volumeBtn.classList.add("on")
+
+  } else if ( volumeBtn.classList.contains("on") ){
+    audio.pause();
+    volumeBtn.innerText = "🔈";
+    volumeBtn.classList.remove("on")
+    volumeBtn.classList.add("off")
+  }
+}
+
+volumeBtn.addEventListener("click", volume)
