aab72639982807835eb786a51818945541db2d8e
developer-KOR <126733809+developer-KOR@users.noreply.github.com>
2023-03-08 14:21:22 +0900

    Add files via upload
    오디오 js 세부사항 수정

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,9 @@
 const audioFile = [
   {
+    title: "Main Theme",
+    src: "audio/Starcraft Main Title.mp3"
+  },
+  {
     title: "Terran 1 BGM",
     src: "audio/Terran One.mp3"
   },
@@ -11,22 +15,49 @@ const audioFile = [
     title: "Terran 3 BGM",
     src: "audio/Terran Three.mp3"
   },
+  {
+    title: "Protoss 1 BGM",
+    src: "audio/Protoss One.mp3"
+  },
+  {
+    title: "Protoss 2 BGM",
+    src: "audio/Protoss Two.mp3"
+  },
+  {
+    title: "Protoss 3 BGM",
+    src: "audio/Protoss Three.mp3"
+  },
+  {
+    title: "Zerg 1 BGM",
+    src: "audio/Zerg One.mp3"
+  },
+  {
+    title: "Zerg 2 BGM",
+    src: "audio/Zerg Three.mp3"
+  },
+  {
+    title: "Zerg 3 BGM",
+    src: "audio/Zerg Three.mp3"
+  },
 ];
 
 const audioIndex = Math.floor( Math.random() * audioFile.length )
 const audioWrap = document.querySelector("#wrap .audio");
 const audio = document.createElement("audio");
+const audioSrc = document.createElement("source");
 const audioTitle = document.createElement("figcaption");
 const volumeBtn = document.querySelector(".volume_button");
 
 const AUTO_PLAY = "autoplay";
 
 audioTitle.innerText = audioFile[audioIndex].title;
-audio.src = audioFile[audioIndex].src;
+audioSrc.src = audioFile[audioIndex].src;
+audioSrc.type = "audio/mp3"
 audio.autoplay = AUTO_PLAY;
 
 audioWrap.prepend(audioTitle);
 audioWrap.appendChild(audio);
+audio.appendChild(audioSrc);
 
 function volume(e){
   e.preventDefault();
